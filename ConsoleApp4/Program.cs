fad93f67907b09adf08fe68cf2d28eb11e738e60
Admin <tem.ashurov@gmail.com>
2023-10-12 22:10:46 +0300

    Сделал первое задание

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -11,16 +11,48 @@ namespace homework5
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static (int, int) Count(char[] text)
+        {
+            char[] rus_vowels = new[]{ 'а', 'я', 'e', 'ё', 'у', 'о', 'и', 'ю', 'э', 'ы' };
+            (int, int) letter_count = (0, 0);
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < rus_vowels.Length; j++)
+                {
+                    if (rus_vowels[j] == text[i])
+                    {
+                        letter_count.Item1++;
+                    }
+                    else
+                    {
+                        letter_count.Item2++;
+                    }
+                }
+            }
+            return letter_count;
+        }
+        
+        static void Task1(string[] text)
+        {
+            FileInfo file = new FileInfo(text[0]);
+
+            char[] char_arr = File.ReadAllText(text[0]).ToLower().ToCharArray();
+
+            (int, int) counter = Count(char_arr);
+
+            Console.WriteLine($"{counter.Item1} гласных, {counter.Item2} согласных");
+        }
+        
+        static void Task2()
         {
             Console.WriteLine("Первая матрица : ");
             int[,] args1 = new int[2, 2];
-            Random random = new Random();   
+            Random random = new Random();
             for (int i = 0; i < 2; i++)
             {
                 for (int j = 0; j < 2; j++)
                 {
-                    args1[i,j] = random.Next(-100,100);
+                    args1[i, j] = random.Next(-100, 100);
                     Console.Write("{0}\t", args1[i, j]);
 
                 }
@@ -32,11 +64,11 @@ namespace homework5
             {
                 for (int h = 0; h < 2; h++)
                 {
-                    args2[g, h] = random.Next(-100,100);
+                    args2[g, h] = random.Next(-100, 100);
                     Console.Write("{0}\t", args2[g, h]);
                 }
                 Console.WriteLine();
-                
+
             }
 
             int[,] result = new int[2, 2];
@@ -54,42 +86,16 @@ namespace homework5
                 }
                 Console.WriteLine();
             }
-            Console.ReadKey();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            }
         }
+        
+        static void Main(string[] text)
+        {
+
+            Task1(text);
+            Task2();
+
+            Console.ReadKey();
+        }
+    }
 }
